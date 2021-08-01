e8fec6c13f354da4edb40fbdbd706c22f00542fa
孤风雪影 <efairy520@qq.com>
2021-08-01 17:35:08 +0800

    轮播图-抽取组件

---
+++ src/components/home/TopSlider.vue
@@ -0,0 +1,38 @@
+/<template>
+  <div class="top-slider">
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="red">
+      <van-swipe-item v-for="(slider, index) in sliders" :key="index">
+        <img
+          :src="slider.pic"
+        />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sliders: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.top-slider {
+  .van-swipe-item {
+    max-height: 187px;
+    background-color: #39a9ed;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 /<template>
   <div class="home">
-      首页
+    <top-slider :sliders="sliderList"></top-slider>
   </div>
 </template>
 
 <script>
+import TopSlider from "@/components/home/TopSlider"
 export default {
-
+  components: {
+    TopSlider,
+  },
+  data() {
+    return {
+      sliderList: [
+        {
+          pic: "https://demo26.crmeb.net/uploads/attach/2021/06/20210609/573443cca089a4103fd5bbc060494c1d.jpg"
+        },
+        {
+          pic: "https://demo26.crmeb.net/uploads/attach/2021/06/20210609/573443cca089a4103fd5bbc060494c1d.jpg"
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
