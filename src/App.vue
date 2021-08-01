2739af1e5a5fd3513f615f7fc0dd059ceb4cc208
孤风雪影 <efairy520@qq.com>
2021-08-01 16:19:06 +0800

    引入Vant-添加Tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
+
     <router-view></router-view>
+
+    <!-- tabbar 底部导航 -->
+    <van-tabbar route active-color="#E1251B">
+      <van-tabbar-item replace to="/home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item replace to="/category" icon="bag-o">分类</van-tabbar-item>
+      <van-tabbar-item replace to="/cart" icon="cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item replace to="/user" icon="manager-o">我的</van-tabbar-item>
+    </van-tabbar>
+
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  data() {
+    return {};
+  },
+  components: {},
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './routers'
+import Vant from 'vant'
 
+import App from '@/App.vue'
+import router from '@/routers'
+
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
